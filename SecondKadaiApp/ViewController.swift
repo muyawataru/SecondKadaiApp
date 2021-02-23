35bbad07ffedf7250ad080c11bf035bc723fcc76
wataru.muya <wmuya0303@gmail.com>
2021-02-23 18:42:16 +0900

    課題再提出

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,20 +10,30 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var btnExec: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        btnExec.isEnabled = false
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // インスタンス化
+        
+        //インスタンス化
         let resultViewController = segue.destination as! ResultViewController
-        // プロパティ変更
+        //プロパティ変更
         resultViewController.userName = txtName.text
+        
     }
     
-    //完了ボタン - 押下時
-    @IBAction func btnExec(_ sender: Any) {
+    //テキストフィールドが変更された時
+    @IBAction func txtNameEditingChanged(_ txtName: UITextField) {
+        
+        if txtName.text == "" {
+            btnExec.isEnabled = false
+            return
+        }
+        btnExec.isEnabled = true
     }
     
     //この画面に戻ってきた時
